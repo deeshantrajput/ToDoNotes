9d68cfc89f2df107b3717254315301c940f83dc8
deeshantrajput <deeshantrajput@gmail.com>
2020-05-08 09:38:18 +0530

    fixes

--- app/src/main/java/com/deeshant/todonotesapp/views/MyNotesActivity.kt
+++ app/src/main/java/com/deeshant/todonotesapp/views/MyNotesActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
@@ -36,20 +37,26 @@ class MyNotesActivity: AppCompatActivity()  {
         bindView()
         setUpSharedPrefrences()
         getIntentData()
-
         gatDatafromDatabase()
-        supportActionBar?.title = fullName
+        setUpToolBartext()
+        setUpClickListener()
+        setUpRecyclerView()
+    }
 
+    private fun setUpClickListener() {
         fabAddNotes.setOnClickListener {
             setUpDialogBox()
         }
     }
 
+    private fun setUpToolBartext() {
+        supportActionBar?.title = Html.fromHtml("<font color='#F29035'>"+fullName+"</font>")
+    }
+
     private fun gatDatafromDatabase() {
         val notesApp = applicationContext as NotesApp
         val notesDao = notesApp.getNotesDb().notesDao()
-
-
+        notesArrayList.addAll(notesDao.getsAll())
     }
 
     private fun setUpDialogBox() {
